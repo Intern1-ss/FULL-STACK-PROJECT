4cfea7d46345a986891b4b376d2848a151763445
Charan Kumar <kumar99charan@gmail.com>
2025-05-24 14:41:44 +0530

    intialrun
    set up student home page and run the program

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -58,15 +58,15 @@ MIDDLEWARE = [
 ROOT_URLCONF = "lms.urls"
 
 TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+   {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
